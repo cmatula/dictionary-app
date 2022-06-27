cdc799662e1d87ef963082786fbf438feffa0bc0
cmatula <90786977+cmatula@users.noreply.github.com>
2022-06-27 18:05:15 +0200

    Added Search input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
+import Dictionary from "./Dictionary";
+
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Logo</header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">
+            Open source code by Cara Matula
+          </footer>
+        </small>
+      </div>
     </div>
   );
 }
