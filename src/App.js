20bc8601c6122ebde03148665d39a12a8f0a616b
cmatula <90786977+cmatula@users.noreply.github.com>
2022-06-28 16:08:45 +0200

    Added logo and link to Github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
 
 import Dictionary from "./Dictionary";
+import logo from "./logo.png";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Logo</header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary />
         </main>
         <small>
           <footer className="App-footer">
-            Open source code by Cara Matula
+            <a
+              href="https://github.com/cmatula/dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open source{" "}
+            </a>{" "}
+            code by Cara Matula
           </footer>
         </small>
       </div>
