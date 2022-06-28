288d713313f75b33be59741c5ed275d13551c8d8
cmatula <90786977+cmatula@users.noreply.github.com>
2022-06-28 15:49:03 +0200

    Added the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary(params) {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
+    setResults(response.data[0]);
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
@@ -27,6 +30,7 @@ export default function Dictionary(params) {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
         <input type="submit" value="Search" />
+        <Results results={results} />
       </form>
     </div>
   );
