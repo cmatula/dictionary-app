fbccc3b786f213721b6ad0ba16635d254aa4f8ee
cmatula <90786977+cmatula@users.noreply.github.com>
2022-07-01 08:41:38 +0200

    Updated title and footer

--- src/App.js
+++ src/App.js
@@ -11,19 +11,27 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <h1>Dictionary</h1>
+          <h1>the Dictionary</h1>
           <Dictionary />
         </main>
         <small>
           <footer className="App-footer">
+            This project is open source coded on{" "}
             <a
               href="https://github.com/cmatula/dictionary-app"
               target="_blank"
               rel="noreferrer"
             >
-              Open source{" "}
+              GitHub{" "}
             </a>{" "}
-            code by Cara Matula
+            by{" "}
+            <a
+              href="https://www.linkedin.com/in/cara-matula-3b0090141"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Cara Matula{" "}
+            </a>
           </footer>
         </small>
       </div>
